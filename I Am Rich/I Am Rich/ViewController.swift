5f0ee328ab6a7f9e6d52e0b93642d8b86f5a6be5
danli-w <51030042+danli-w@users.noreply.github.com>
2019-10-22 22:16:48 -0500

    Done

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Danli Wang on 8/18/19.
+//  Copyright © 2019 Danli Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
